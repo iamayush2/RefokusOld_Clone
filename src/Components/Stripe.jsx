e82c8123f831509dab3b7e1bc8d6f0d4b36c6ec3
Ayush Dulhani <dulhaniayush12@gmail.com>
2024-09-12 16:53:58 +0530

    stripes responsive bug fixed

--- src/Components/Stripe.jsx
+++ src/Components/Stripe.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Stripe = ({ src, no, imgwidth }) => {
   return (
-    <div className="  w-[19%]  h-[9vh] gap-2 border-[0.2px] border-zinc-600 flex items-center p-4">
+    <div className=" w-[160%] sm:w-[19%]  h-[9vh] gap-2 border-[0.2px] border-zinc-600 flex items-center p-2 sm:p-4">
       <div className={`w-[90%] h-full flex items-center  `}>
-        <img src={src} alt="" className={`w-[${imgwidth}]`} />
+        <img src={src} alt="" className={`w-[95%] sm:w-[${imgwidth}]`} />
       </div>
-      <p className="font-bold text-white">{no}</p>
+      <p className=" text-xs sm:text-base sm:font-bold text-white">{no}</p>
     </div>
   );
 };
--- src/Components/StripeCont.jsx
+++ src/Components/StripeCont.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Stripe from "./Stripe";
+import { div } from "framer-motion/client";
 
 const StripeCont = () => {
-  let screenWidth = screen.width;
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const updateScreenWidth = () => {
+    setScreenWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateScreenWidth);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", updateScreenWidth);
+    };
+  }, []);
 
   return (
     <div className="w-full  mt-[5vh] flex items-center justify-center ">
@@ -27,27 +41,32 @@ const StripeCont = () => {
         no={15}
         imgwidth={"80%"}
       />
-      <Stripe
-        src={
-          "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd7cf6b3689fb_css-design-awards-logos-id1L9L8Yvp%201.svg"
-        }
-        no={52}
-        imgwidth={"90%"}
-      />
-      <Stripe
-        src={
-          "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63d6e83420934a94d642103b_NCC2021_LogoLockup%201.svg"
-        }
-        no={2}
-        imgwidth={"60%"}
-      />
-      <Stripe
-        src={
-          "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd75d563689fd_Awwards-logotype-2018%201.svg"
-        }
-        no={15}
-        imgwidth={"80%"}
-      />
+      {screenWidth > 550 ? (
+        <>
+          {" "}
+          <Stripe
+            src={
+              "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd7cf6b3689fb_css-design-awards-logos-id1L9L8Yvp%201.svg"
+            }
+            no={52}
+            imgwidth={"90%"}
+          />
+          <Stripe
+            src={
+              "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63d6e83420934a94d642103b_NCC2021_LogoLockup%201.svg"
+            }
+            no={2}
+            imgwidth={"60%"}
+          />
+          <Stripe
+            src={
+              "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd75d563689fd_Awwards-logotype-2018%201.svg"
+            }
+            no={15}
+            imgwidth={"80%"}
+          />
+        </>
+      ) : null}
     </div>
   );
 };
